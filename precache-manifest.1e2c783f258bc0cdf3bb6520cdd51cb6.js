64126f8cfa02e2fb86e4462a04eadf49301f93b9
unknown <alerpace@o2.pl>
2020-03-26 14:47:32 +0100

    Updates

---
+++ precache-manifest.1e2c783f258bc0cdf3bb6520cdd51cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddafd48b370a57244e1b68312536f38",
+    "url": "/pogoda/index.html"
+  },
+  {
+    "revision": "a6fe1f77838300d9eb7e",
+    "url": "/pogoda/static/css/main.5d3292b0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/pogoda/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a6fe1f77838300d9eb7e",
+    "url": "/pogoda/static/js/main.48a404fe.chunk.js"
+  },
+  {
+    "revision": "bdb4c9d2324581a2858d",
+    "url": "/pogoda/static/js/runtime~main.a579aba6.js"
+  }
+]);
